05861315d48e380c2782f54bf4bc860742ee0196
Miguel-T1 <108372@aluno.uricer.edu.br>
2026-03-09 23:48:09 -0300

    Add files via upload

---
+++ ImageLoader/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace ImageLoader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btCarregarImagem = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btSalvarImagem = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.carregar2 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btSomarImagem = new System.Windows.Forms.Button();
+            this.btSubtImagem = new System.Windows.Forms.Button();
+            this.BrilhoUp = new System.Windows.Forms.NumericUpDown();
+            this.Abrilho = new System.Windows.Forms.Button();
+            this.BrilhoDown = new System.Windows.Forms.NumericUpDown();
+            this.Dbrilho = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BrilhoUp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BrilhoDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btCarregarImagem
+            // 
+            this.btCarregarImagem.Location = new System.Drawing.Point(37, 184);
+            this.btCarregarImagem.Name = "btCarregarImagem";
+            this.btCarregarImagem.Size = new System.Drawing.Size(163, 37);
+            this.btCarregarImagem.TabIndex = 0;
+            this.btCarregarImagem.Text = "Carregar Imagem";
+            this.btCarregarImagem.UseVisualStyleBackColor = true;
+            this.btCarregarImagem.Click += new System.EventHandler(this.btCarregarImagem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(202, 161);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(931, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(202, 161);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btSalvarImagem
+            // 
+            this.btSalvarImagem.Location = new System.Drawing.Point(947, 184);
+            this.btSalvarImagem.Name = "btSalvarImagem";
+            this.btSalvarImagem.Size = new System.Drawing.Size(163, 37);
+            this.btSalvarImagem.TabIndex = 3;
+            this.btSalvarImagem.Text = "Salvar Imagem";
+            this.btSalvarImagem.UseVisualStyleBackColor = true;
+            this.btSalvarImagem.Click += new System.EventHandler(this.btSalvarImagem_Click);
+            // 
+            // carregar2
+            // 
+            this.carregar2.Location = new System.Drawing.Point(300, 184);
+            this.carregar2.Name = "carregar2";
+            this.carregar2.Size = new System.Drawing.Size(163, 37);
+            this.carregar2.TabIndex = 4;
+            this.carregar2.Text = "Carregar Imagem";
+            this.carregar2.UseVisualStyleBackColor = true;
+            this.carregar2.Click += new System.EventHandler(this.carregar2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(280, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(202, 161);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // btSomarImagem
+            // 
+            this.btSomarImagem.Location = new System.Drawing.Point(168, 261);
+            this.btSomarImagem.Name = "btSomarImagem";
+            this.btSomarImagem.Size = new System.Drawing.Size(163, 37);
+            this.btSomarImagem.TabIndex = 6;
+            this.btSomarImagem.Text = "Somar Imagem";
+            this.btSomarImagem.UseVisualStyleBackColor = true;
+            this.btSomarImagem.Click += new System.EventHandler(this.btSomarImagem_Click);
+            // 
+            // btSubtImagem
+            // 
+            this.btSubtImagem.Location = new System.Drawing.Point(168, 315);
+            this.btSubtImagem.Name = "btSubtImagem";
+            this.btSubtImagem.Size = new System.Drawing.Size(163, 37);
+            this.btSubtImagem.TabIndex = 7;
+            this.btSubtImagem.Text = "Subtrair Imagem";
+            this.btSubtImagem.UseVisualStyleBackColor = true;
+            this.btSubtImagem.Click += new System.EventHandler(this.btSubtImagem_Click);
+            // 
+            // BrilhoUp
+            // 
+            this.BrilhoUp.Location = new System.Drawing.Point(499, 30);
+            this.BrilhoUp.Name = "BrilhoUp";
+            this.BrilhoUp.Size = new System.Drawing.Size(120, 20);
+            this.BrilhoUp.TabIndex = 8;
+            this.BrilhoUp.ValueChanged += new System.EventHandler(this.BrilhoUp_ValueChanged);
+            // 
+            // Abrilho
+            // 
+            this.Abrilho.Location = new System.Drawing.Point(643, 20);
+            this.Abrilho.Name = "Abrilho";
+            this.Abrilho.Size = new System.Drawing.Size(163, 37);
+            this.Abrilho.TabIndex = 9;
+            this.Abrilho.Text = "Aumentar brilho";
+            this.Abrilho.UseVisualStyleBackColor = true;
+            this.Abrilho.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BrilhoDown
+            // 
+            this.BrilhoDown.Location = new System.Drawing.Point(499, 85);
+            this.BrilhoDown.Name = "BrilhoDown";
+            this.BrilhoDown.Size = new System.Drawing.Size(120, 20);
+            this.BrilhoDown.TabIndex = 10;
+            this.BrilhoDown.ValueChanged += new System.EventHandler(this.BrilhoDown_ValueChanged);
+            // 
+            // Dbrilho
+            // 
+            this.Dbrilho.Location = new System.Drawing.Point(643, 75);
+            this.Dbrilho.Name = "Dbrilho";
+            this.Dbrilho.Size = new System.Drawing.Size(163, 37);
+            this.Dbrilho.TabIndex = 11;
+            this.Dbrilho.Text = "Diminuir brilho";
+            this.Dbrilho.UseVisualStyleBackColor = true;
+            this.Dbrilho.Click += new System.EventHandler(this.Dbrilho_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1163, 562);
+            this.Controls.Add(this.Dbrilho);
+            this.Controls.Add(this.BrilhoDown);
+            this.Controls.Add(this.Abrilho);
+            this.Controls.Add(this.BrilhoUp);
+            this.Controls.Add(this.btSubtImagem);
+            this.Controls.Add(this.btSomarImagem);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.carregar2);
+            this.Controls.Add(this.btSalvarImagem);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btCarregarImagem);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BrilhoUp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BrilhoDown)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btCarregarImagem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btSalvarImagem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button carregar2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btSomarImagem;
+        private System.Windows.Forms.Button btSubtImagem;
+        private System.Windows.Forms.NumericUpDown BrilhoUp;
+        private System.Windows.Forms.Button Abrilho;
+        private System.Windows.Forms.NumericUpDown BrilhoDown;
+        private System.Windows.Forms.Button Dbrilho;
+    }
+}
+
---
+++ ImageLoader/Form1.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageLoader
+{
+    public partial class Form1 : Form
+    {
+
+        Bitmap img1;
+        Bitmap img2;
+        Bitmap img3;
+        Bitmap img4;
+        int brilhoAtual = 0;
+        byte[,] somarsomarR;
+        byte[,] somarsomarG;
+        byte[,] somarsomarB;
+        byte[,] vImg1Gray;
+
+        byte[,] vImg1R;
+        byte[,] vImg1G;
+        byte[,] vImg1B;
+        byte[,] vImg1A;
+
+
+
+
+        public Form1()
+        {
+           
+            InitializeComponent();
+        }
+        
+
+        private void btCarregarImagem_Click(object sender, EventArgs e)
+        {
+            // Configurações iniciais da OpenFileDialogBox
+            var filePath = string.Empty;
+            openFileDialog1.InitialDirectory = "C:\\Matlab";
+            openFileDialog1.Filter = "TIFF image (*.tif)|*.tif|JPG image (*.jpg)|*.jpg|BMP image (*.bmp)|*.bmp|PNG image (*.png)|*.png|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            // Se um arquivo foi localizado com sucesso...
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Armnazena o path do arquivo de imagem
+                filePath = openFileDialog1.FileName;
+
+
+                bool bLoadImgOK = false;
+                try
+                {
+                    img1 = new Bitmap(filePath);
+
+                    bLoadImgOK = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Erro ao abrir imagem...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    bLoadImgOK = false;
+                }
+
+                // Se a imagem carregou perfeitamente...
+                if (bLoadImgOK == true)
+                {
+                    // Adiciona imagem na PictureBox
+                    pictureBox1.Image = img1;
+                    /* vImg1Gray = new byte[img1.Width, img1.Height];
+                     vImg1R = new byte[img1.Width, img1.Height];
+                     vImg1G = new byte[img1.Width, img1.Height];
+                     vImg1B = new byte[img1.Width, img1.Height];
+                     vImg1A = new byte[img1.Width, img1.Height];
+
+                     // Percorre todos os pixels da imagem...
+                     for (int i = 0; i < img1.Width; i++)
+                     {
+                         for (int j = 0; j < img1.Height; j++)
+                         {
+                             Color pixel = img1.GetPixel(i, j);
+
+                             // Para imagens em escala de cinza, extrair o valor do pixel com...
+                             //byte pixelIntensity = Convert.ToByte((pixel.R + pixel.G + pixel.B) / 3);
+                             byte pixelIntensity = Convert.ToByte((pixel.R + pixel.G + pixel.B) / 3);
+                             vImg1Gray[i, j] = pixelIntensity;
+
+                             // Para imagens RGB, extrair o valor do pixel com...
+                             byte R = pixel.R;
+                             byte G = pixel.G;
+                             byte B = pixel.B;
+                             byte A = pixel.A;
+
+                             vImg1R[i, j] = R;
+                             vImg1G[i, j] = G;
+                             vImg1B[i, j] = B;
+                             vImg1A[i, j] = A;
+
+                             Color cor = Color.FromArgb(
+                                 255,
+                                 vImg1Gray[i, j],
+                                 vImg1Gray[i, j],
+                                 vImg1Gray[i, j]);
+
+                             img2.SetPixel(i, j, cor);
+                         }
+                     }
+
+                    pictureBox2.Image = img2;
+                */
+                }
+
+            }
+        }
+
+        private void btSalvarImagem_Click(object sender, EventArgs e)
+        {
+            if (img2 == null)
+                return;
+
+            // Configurações iniciais da saveFileDialog1
+            var filePath = string.Empty;
+            saveFileDialog1.InitialDirectory = "c:\\";
+            saveFileDialog1.Filter = "TIFF image (*.tif)|*.tif|JPG image (*.jpg)|*.jpg|BMP image (*.bmp)|*.bmp|PNG image (*.png)|*.png|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+
+            ImageFormat format = img2.RawFormat;
+
+            if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string ext = System.IO.Path.GetExtension(saveFileDialog1.FileName);
+                switch (ext)
+                {
+                    case ".jpg":
+                        format = ImageFormat.Jpeg;
+                        break;
+                    case ".bmp":
+                        format = ImageFormat.Bmp;
+                        break;
+                    case ".tif":
+                        format = ImageFormat.Tiff;
+                        break;
+                    case ".png":
+                        format = ImageFormat.Png;
+                        break;
+                }
+
+                //pictureBox3.Image.Save(saveFileDialog1.FileName, format);
+                img2.Save(saveFileDialog1.FileName, format);
+            }
+        }
+
+      
+        private void carregar2_Click(object sender, EventArgs e)
+        {
+            var filePath = string.Empty;
+            openFileDialog1.InitialDirectory = "C:\\Matlab";
+            openFileDialog1.Filter = "TIFF image (*.tif)|*.tif|JPG image (*.jpg)|*.jpg|BMP image (*.bmp)|*.bmp|PNG image (*.png)|*.png|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog1.FileName;
+
+                bool bLoadImgOK = false;
+
+                try
+                {
+                    img3 = new Bitmap(filePath);
+                    bLoadImgOK = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Erro ao abrir imagem...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    bLoadImgOK = false;
+                }
+
+                if (bLoadImgOK == true)
+                {
+                    pictureBox3.Image = img3;
+                }
+            }
+        }
+        private void btSomarImagem_Click(object sender, EventArgs e)
+        {
+            if (img1 == null || img3 == null)
+            {
+                MessageBox.Show("Carregue as duas imagens primeiro.");
+                return;
+            }
+
+            if (img1.Width != img3.Width || img1.Height != img3.Height)
+            {
+                MessageBox.Show("As imagens precisam ter o mesmo tamanho!");
+                return;
+            }
+
+            img4 = new Bitmap(img1.Width, img1.Height);
+
+            for (int i = 0; i < img1.Width; i++)
+            {
+                for (int j = 0; j < img1.Height; j++)
+                {
+                    Color pixel = img1.GetPixel(i, j);
+                    Color pixel3 = img3.GetPixel(i, j);
+
+                    int R = pixel.R + pixel3.R;
+                    int G = pixel.G + pixel3.G;
+                    int B = pixel.B + pixel3.B;
+                    int A = pixel.A + pixel3.A;
+
+                    if (R > 255) R = 255;
+                    if (G > 255) G = 255;
+                    if (B > 255) B = 255;
+                    if (A > 255) A = 255;
+
+                    Color cor = Color.FromArgb(A, R, G, B);
+
+                    img4.SetPixel(i, j, cor);
+                }
+            }
+
+            pictureBox2.Image = img4;
+        }
+
+        private void btSubtImagem_Click(object sender, EventArgs e)
+        {
+            if (img1 == null || img3 == null)
+            {
+                MessageBox.Show("Carregue as duas imagens primeiro.");
+                return;
+            }
+
+            if (img1.Width != img3.Width || img1.Height != img3.Height)
+            {
+                MessageBox.Show("As imagens precisam ter o mesmo tamanho!");
+                return;
+            }
+            img4 = new Bitmap(img1.Width, img1.Height);
+
+            for (int i = 0; i < img1.Width; i++)
+            {
+                for (int j = 0; j < img1.Height; j++)
+                {
+                    Color pixel = img1.GetPixel(i, j);
+                    Color pixel3 = img3.GetPixel(i, j);
+
+                    int R = pixel.R - pixel3.R;
+                    int G = pixel.G - pixel3.G;
+                    int B = pixel.B - pixel3.B;
+                   
+
+                    if (R < 0) R = 0;
+                    if (G < 0) G = 0;
+                    if (B < 0) B = 0;
+                
+
+                    Color cor = Color.FromArgb( R, G, B);
+
+                    img4.SetPixel(i, j, cor);
+                }
+            }
+
+            pictureBox2.Image = img4;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap resultado = new Bitmap(img1.Width, img1.Height);
+
+            for (int i = 0; i < img1.Width; i++)
+            {
+                for (int j = 0; j < img1.Height; j++)
+                {
+                    Color pixel = img1.GetPixel(i, j);
+
+                    int R = pixel.R + brilhoAtual;
+                    int G = pixel.G + brilhoAtual;
+                    int B = pixel.B + brilhoAtual;
+
+                    if (R > 255) R = 255;
+                    if (G > 255) G = 255;
+                    if (B > 255) B = 255;
+
+                    resultado.SetPixel(i, j, Color.FromArgb(R, G, B));
+                }
+            }
+
+            pictureBox2.Image = resultado;
+        }
+         
+        
+
+      
+private void BrilhoUp_ValueChanged(object sender, EventArgs e)
+        {
+            brilhoAtual = (int)BrilhoUp.Value;
+        }
+
+        private void BrilhoDown_ValueChanged(object sender, EventArgs e)
+        {
+            brilhoAtual = (int)BrilhoDown.Value;
+        }
+
+        private void Dbrilho_Click(object sender, EventArgs e)
+        {
+            Bitmap resultado = new Bitmap(img1.Width, img1.Height);
+
+            for (int i = 0; i < img1.Width; i++)
+            {
+                for (int j = 0; j < img1.Height; j++)
+                {
+                    Color pixel = img1.GetPixel(i, j);
+
+                    int R = pixel.R - brilhoAtual;
+                    int G = pixel.G - brilhoAtual;
+                    int B = pixel.B - brilhoAtual;
+
+                    if (R < 0) R = 0;
+                    if (G < 0) G = 0;
+                    if (B < 0) B = 0;
+
+                    resultado.SetPixel(i, j, Color.FromArgb(R, G, B));
+                }
+            }
+
+            pictureBox2.Image = resultado;
+        }
+    }
+    }
+
+        
+    
+
